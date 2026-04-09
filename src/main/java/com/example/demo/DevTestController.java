19022146038a455bc9faa959f0551613b7c7d485
tahiralamr <YOUR_GITHUB_EMAIL>
2026-04-09 12:26:30 +0530

    added api getHome

--- src/main/java/com/example/demo/DevTestController.java
+++ src/main/java/com/example/demo/DevTestController.java
@@ -1,4 +1,11 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class DevTestController {
+
+    @GetMapping("/home")
+    public  String getHome(){
+        return "Home Page";
+    }
 }
